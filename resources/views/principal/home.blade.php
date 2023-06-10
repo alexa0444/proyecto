a09d59aa2637337fc707f255d754e5bd2c45c9bd
alexac <2190006@ipisa.edu.do>
2023-06-10 19:11:51 -0400

    Cambios en el menu

--- resources/views/principal/home.blade.php
+++ resources/views/principal/home.blade.php
@@ -1,90 +1,133 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="icon" type="image/x-icon" href="{{ asset('img/icon.png') }}">
-    <title>TOVO</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
-<style>
+    <title>TOVO</title>  
 
-.img{
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 90vh; 
-}
-
-
-</style>
-
-  </head>
-
-<body>
-
-
-    
-      
-
-
-      
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous"> --}}
   
-          @if (Auth::check() && Auth::user()->admin == 1)
-          <nav class="navbar navbar-expand-lg bg-body-tertiary">
-            <div class="container-fluid">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script> 
+
+
+
+
+@if (Auth::check() && Auth::user()->admin == 1)
+<header>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+
+  <!-- Navbar -->
+  <nav class="navbar navbar-expand-lg navbar-light bg-white fixed-top">
+    <div class="container-fluid">
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-mdb-toggle="collapse"
+        data-mdb-target="#navbarExample01"
+        aria-controls="navbarExample01"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <i class="fas fa-bars"></i>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarExample01">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item active">
+            <a class="nav-link active" aria-current="page"href="{{ route('prestamos.index') }}">Control de usuarios</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page"href="{{ route('librosindex.principal') }}">Control de compras</a>
+          </li>
+    
         
-              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <div
+    class="p-5 text-center bg-image"
+    style="
+      background-image: url('https://i.pinimg.com/564x/0a/7b/12/0a7b125a3236a2e77f447c6a436dd7ee.jpg');
+      height: 400px;
+      margin-top: 58px;
+    "
+  >
+    <div class="mask" style="background-color: rgba(97, 97, 97, 0.6);">
+      <div class="d-flex justify-content-center align-items-center h-100">
+        <div class="text-white">
+          <h1 class="mb-3">TOVO</h1>
+          <h4 class="mb-3">Tareas organizadas, vida ordenada</h4>
+
+
          
-              <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div class="navbar-nav">
-                  <a class="nav-link active" aria-current="page"href="{{ route('librosindex.principal') }}">Control de compras</a>
-                  <a class="nav-link active" aria-current="page"href="{{ route('prestamos.index') }}">Control de usuarios</a>
-                 
-      
-      
-      <h3>administrador</h3>
-
-      
-
-                </div>
-              </div>
-            </div>
-          </nav>
-  
-      @elseif (Auth::check())
-
-      <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-    
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-     
-          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav">
-              <a class="nav-link active" aria-current="page"href="{{ route('solicitar') }}">Lista de tareas</a>
-              <a class="nav-link active" aria-current="page"href="{{ route('calendario') }}">Calendario</a>
-              <a class="nav-link active" aria-current="page"href="{{ route('solicitar') }}">Compra</a>
-  
-  <h3>usuario</h3>
-  
-            </div>
-          </div>
         </div>
-      </nav>
+      </div>
+    </div>
+  </div>
+  <h3>este es el administrador</h3>
+  <h2>poner en esta parte beneficios de organizacion</h2>
+
+</header>
+
+@elseif (Auth::check())
+<header>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+
+  <!-- Navbar -->
+  <nav class="navbar navbar-expand-lg navbar-light bg-white fixed-top">
+    <div class="container-fluid">
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-mdb-toggle="collapse"
+        data-mdb-target="#navbarExample01"
+        aria-controls="navbarExample01"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <i class="fas fa-bars"></i>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarExample01">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item active">
+            <a class="nav-link active" aria-current="page"href="{{ route('solicitar') }}">Lista de tareas</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page"href="{{ route('solicitar') }}">Compra</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page"href="{{ route('calendario') }}">Calendario</a>
+          </li>
+          
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <div
+    class="p-5 text-center bg-image"
+    style="
+      background-image: url('https://i.pinimg.com/564x/0a/7b/12/0a7b125a3236a2e77f447c6a436dd7ee.jpg');
+      height: 400px;
+      margin-top: 58px;
+    "
+  >
+    <div class="mask" style="background-color: rgba(97, 97, 97, 0.6);">
+      <div class="d-flex justify-content-center align-items-center h-100">
+        <div class="text-white">
+          <h1 class="mb-3">TOVO</h1>
+          <h4 class="mb-3">Tareas organizadas, vida ordenada</h4>
+
+
          
-
-      @endif
-
-
-  <br>
-<br>
-<br>
-@extends('layouts.footer')
-</body>
-</html>
+        </div>
+      </div>
+    </div>
+  </div>
+  <h3>este es el usuario</h3>
+  <h2>poner en esta parte beneficios de organizacion</h2>
+@endif
+</header>
